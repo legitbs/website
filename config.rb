9447467ce73e0381e3e7e1ecbfc852133b4302ec
Vito Genovese <vito@legitbs.net>
2014-12-07 17:58:16 -0500

    ohai middleman

--- config.rb
+++ config.rb
@@ -24,6 +24,10 @@
 #   page "/admin/*"
 # end
 
+with_layout false do
+  page '**'
+end
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
@@ -36,9 +40,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
