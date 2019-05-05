ec3258b0747524e25a562418910e36aa44ba476e
Vito Genovese <vito@legitbs.net>
2019-05-05 13:23:24 -0400

    start on transit map

--- config.rb
+++ config.rb
@@ -12,6 +12,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+ignore '/statdump*'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
