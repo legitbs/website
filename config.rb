0ce7635ba2e0643be90ade9f4a4eb59b4a462415
Vito Genovese <vito@legitbs.net>
2019-05-09 19:53:34 -0400

    fix statdump

--- config.rb
+++ config.rb
@@ -12,8 +12,6 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-ignore '/statdump*'
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
